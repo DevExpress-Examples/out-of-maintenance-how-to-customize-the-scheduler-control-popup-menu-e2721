cd18cc399f32f1271b6ae6dd458a165a90b71ccc
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/PopupMenuCustomization/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Windows;
+using DevExpress.Xpf.Bars;
+using DevExpress.Xpf.Scheduler;
+using DevExpress.Xpf.Scheduler.Menu;
+using DevExpress.Xpf.Scheduler.UI;
+using DevExpress.XtraScheduler;
+
+namespace CustomMenu {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Appointment apt = schedulerControl1.Storage.CreateAppointment(AppointmentType.Normal, DateTime.Now, DateTime.Now.AddHours(1.5));
+            schedulerControl1.Storage.AppointmentStorage.Add(apt);
+            schedulerControl1.ActiveView.GotoTimeInterval(new TimeInterval(DateTime.Now.AddHours(-2), DateTime.Now.AddHours(-1)));
+        }
+
+        #region #PopupMenuShowing
+        private void schedulerControl1_PopupMenuShowing(object sender, SchedulerMenuEventArgs e) {
+            // Check whether this event was raised for a default popup menu of the Scheduler control.
+            if (e.Menu.Name == SchedulerMenuItemName.DefaultMenu)
+            {
+                for (int i = 0; i < e.Menu.Items.Count; i++)
+                {
+                    SchedulerBarItem menuItem = e.Menu.Items[i] as SchedulerBarItem;
+                    if (menuItem != null)
+                    {
+                        if (menuItem != null && menuItem.Name == SchedulerMenuItemName.NewAllDayEvent)
+                        {
+                            menuItem.Content = "New Task for the Entire Day";
+                            break;
+                        }
+                    }
+                }
+
+                // Remove the New Recurring Event menu item using the Action approach.
+                e.Customizations.Add(new RemoveBarItemAndLinkAction()
+                {
+                    ItemName = SchedulerMenuItemName.NewRecurringEvent
+                });
+
+                // Create a custom menu item.
+                BarButtonItem myMenuItem = new BarButtonItem();
+                myMenuItem.Name = "customItem";
+                myMenuItem.Content = "Item Added in CodeBehind";
+                myMenuItem.ItemClick += new ItemClickEventHandler(customItem_ItemClick);
+
+                // Insert a menu separator.
+                e.Customizations.Add(new BarItemLinkSeparator());
+                // Insert a new item into the Scheduler popup menu.
+                e.Customizations.Add(myMenuItem);
+            } 
+        }
+
+        private void customItem_ItemClick(object sender, ItemClickEventArgs e) {
+            // Implement a custom action.
+            // ...
+        }
+        #endregion #PopupMenuShowing
+
+        #region #AppointmentPopupMenuCustomization
+        private void customCheckItem_CheckedChanged(object sender, ItemClickEventArgs e) {
+            // Implement a custom action.
+            // ...
+        }
+
+        private void customButtonItem_ItemClick(object sender, ItemClickEventArgs e) {
+            // Implement a custom action.
+            // ...
+        }
+        #endregion #AppointmentPopupMenuCustomization
+
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to customize the Scheduler control popup menu
+
+
+<p>The following example illustrates how to customize the Scheduler control popup menus by removing existing menu items and adding new items to the <strong>Default Popup Menu</strong> and <strong>Appointment Popup Menu</strong>.<br>In XAML, add new <a href="http://help.devexpress.com/#WPF/clsDevExpressXpfBarsBarItemtopic">BarItem elements</a> and <a href="http://help.devexpress.com/#WPF/clsDevExpressXpfBarsRemoveBarItemAndLinkActiontopic">RemoveBarItemAndLinkAction</a> actions to the <a href="http://help.devexpress.com/#WPF/DevExpressXpfSchedulerSchedulerControl_DefaultMenuCustomizationstopic">DefaultMenuCustomizations</a> and <a href="http://help.devexpress.com/#WPF/DevExpressXpfSchedulerSchedulerControl_AppointmentMenuCustomizationstopic">AppointmentMenuCustomizations</a> collections.<br>At runtime, handle the <a href="http://help.devexpress.com/#WPF/DevExpressXpfSchedulerSchedulerControl_PopupMenuShowingtopic">PopupMenuShowing</a> event and add new <a href="http://help.devexpress.com/#WPF/clsDevExpressXpfBarsBarItemtopic">BarItem elements</a> and <a href="http://help.devexpress.com/#WPF/clsDevExpressXpfBarsRemoveBarItemAndLinkActiontopic">RemoveBarItemAndLinkAction</a> actions to the <a href="http://help.devexpress.com/#WPF/DevExpressXpfSchedulerSchedulerMenuEventArgs_Customizationstopic">e.Customizations</a> collection.<br><br><img src="https://raw.githubusercontent.com/DevExpress-Examples/how-to-customize-the-scheduler-control-popup-menu-e2721/15.2.4+/media/f4c1eff8-7c68-4895-9d5e-1b508f8e578c.png"><br><br></p>
+
+<br/>
+
+
