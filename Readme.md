a6632cdd8bb5b1426d1c794e46511fef320defc9
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to customize the Scheduler control popup menu
+
+
+<p>The following example illustrates how to customize the Scheduler control popup menus by removing existing menu items and adding new items to the <strong>Default Popup Menu</strong> and <strong>Appointment Popup Menu</strong>.<br>In XAML, add new <a href="http://help.devexpress.com/#WPF/clsDevExpressXpfBarsBarItemtopic">BarItem elements</a> and <a href="http://help.devexpress.com/#WPF/clsDevExpressXpfBarsRemoveBarItemAndLinkActiontopic">RemoveBarItemAndLinkAction</a> actions to the <a href="http://help.devexpress.com/#WPF/DevExpressXpfSchedulerSchedulerControl_DefaultMenuCustomizationstopic">DefaultMenuCustomizations</a> and <a href="http://help.devexpress.com/#WPF/DevExpressXpfSchedulerSchedulerControl_AppointmentMenuCustomizationstopic">AppointmentMenuCustomizations</a> collections.<br>At runtime, handle the <a href="http://help.devexpress.com/#WPF/DevExpressXpfSchedulerSchedulerControl_PopupMenuShowingtopic">PopupMenuShowing</a> event and add new <a href="http://help.devexpress.com/#WPF/clsDevExpressXpfBarsBarItemtopic">BarItem elements</a> and <a href="http://help.devexpress.com/#WPF/clsDevExpressXpfBarsRemoveBarItemAndLinkActiontopic">RemoveBarItemAndLinkAction</a> actions to the <a href="http://help.devexpress.com/#WPF/DevExpressXpfSchedulerSchedulerMenuEventArgs_Customizationstopic">e.Customizations</a> collection.<br><br><img src="https://raw.githubusercontent.com/DevExpress-Examples/how-to-customize-the-scheduler-control-popup-menu-e2721/13.1.4+/media/f4c1eff8-7c68-4895-9d5e-1b508f8e578c.png"><br><br></p>
+
+
+<h3>Description</h3>
+
+<p>Popup menu customization can be performed either at design time (via the XAML markup), or at runtime (via the code-behind file).<br /> To modify a popup menu at runtime, handle the <strong>SchedulerControl.PopupMenuShowing</strong> event. In this event handler, all modifications to the list of menu items can be made by changing the <strong>SchedulerMenuEventArgs.Customizations</strong> collection. To get access to a popup menu for which the event has been raised, use the <strong>SchedulerMenuEventArgs.Menu</strong> property. <br /> To customize the Scheduler control default and appointment popup menus at design time, you can also use the <strong>SchedulerControl.DefaultMenuCustomizations</strong> and <strong>SchedulerControl.AppointmentMenuCustomizations</strong> properties respectively.<br /> In both cases, to remove a specific menu item along with its link, use the <strong>RemoveBarItemAndLinkAction</strong> instance, and to add a custom menu item, add a new <strong>BarButtonItem</strong> or <strong>BarCheckItem</strong> object to the Customizations collection.</p>
+
+<br/>
+
+
---
+++ VB/PopupMenuCustomization/MainWindow.xaml.vb
@@ -0,0 +1,73 @@
+﻿Imports DevExpress.Xpf.Bars
+Imports DevExpress.Xpf.Scheduler
+Imports DevExpress.Xpf.Scheduler.Menu
+Imports DevExpress.Xpf.Scheduler.UI
+Imports DevExpress.XtraScheduler
+Imports System
+Imports System.Windows
+
+Namespace CustomMenu
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            schedulerControl1.Storage.AppointmentStorage.Add(New Appointment(Date.Now, Date.Now.AddHours(1.5)))
+            schedulerControl1.ActiveView.GotoTimeInterval(New TimeInterval(Date.Now.AddHours(-2), Date.Now.AddHours(-1)))
+        End Sub
+
+        #Region "#PopupMenuShowing"
+        Private Sub schedulerControl1_PopupMenuShowing(ByVal sender As Object, ByVal e As SchedulerMenuEventArgs)
+            ' Check whether this event was raised for a default popup menu of the Scheduler control.
+            If e.Menu.Name = SchedulerMenuItemName.DefaultMenu Then
+                For i As Integer = 0 To e.Menu.ItemLinks.Count - 1
+                    Dim menuItemLink As BarButtonItemLink = TryCast(e.Menu.ItemLinks(i), BarButtonItemLink)
+                    If menuItemLink IsNot Nothing Then
+                        Dim menuItem As SchedulerBarItem = TryCast(menuItemLink.Item, SchedulerBarItem)
+                        If menuItem IsNot Nothing AndAlso menuItem.Name = SchedulerMenuItemName.NewAllDayEvent Then
+                            menuItem.Content = "New Task for the Entire Day"
+                            Exit For
+                        End If
+                    End If
+                Next i
+
+                ' Remove the New Recurring Event menu item using the Action approach.
+                e.Customizations.Add(New RemoveBarItemAndLinkAction() With {.ItemName = SchedulerMenuItemName.NewRecurringEvent})
+
+                ' Create a custom menu item.
+                Dim myMenuItem As New BarButtonItem()
+                myMenuItem.Name = "customItem"
+                myMenuItem.Content = "Item Added in CodeBehind"
+                AddHandler myMenuItem.ItemClick, AddressOf customItem_ItemClick
+
+                ' Insert a menu separator.
+                e.Customizations.Add(New BarItemLinkSeparator())
+                ' Insert a new item into the Scheduler popup menu.
+                e.Customizations.Add(myMenuItem)
+            End If
+        End Sub
+
+        Private Sub customItem_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs)
+            ' Implement a custom action.
+            ' ...
+        End Sub
+        #End Region ' #PopupMenuShowing
+
+        #Region "#AppointmentPopupMenuCustomization"
+        Private Sub customCheckItem_CheckedChanged(ByVal sender As Object, ByVal e As ItemClickEventArgs)
+            ' Implement a custom action.
+            ' ...
+        End Sub
+
+        Private Sub customButtonItem_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs)
+            ' Implement a custom action.
+            ' ...
+        End Sub
+        #End Region ' #AppointmentPopupMenuCustomization
+
+
+    End Class
+End Namespace
